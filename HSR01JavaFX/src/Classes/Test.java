5f1dcb35d87e7b3703b57956601edfb6f744339f
BramMenning <brammenning@hotmail.com>
2016-05-10 14:53:55 +0200

    Merge origin/Gijs into Bram
    Conflicts:
    	HSR01PHP/registreren.php

--- HSR01JavaFX/src/Classes/Test.java
+++ HSR01JavaFX/src/Classes/Test.java
@@ -10,17 +10,33 @@ public class Test {
         try {
             Class.forName(config.DRIVER).newInstance();
             conn = DriverManager.getConnection(config.URL, config.USERNAME, config.PASSWORD);
-            //System.out.println("[OUTPUT FROM SELECT]");
-            //String query = "INSERT INTO persoon (type_id,voornaam,achternaam,postcode,huisnummer) VALUES (1,'Martijn','Hacker','0001AA','01')";
-            String query = "SELECT * FROM persoon;";
+            
+            //Inserts
+            String ontvangerInsert = "INSERT INTO ontvanger (idontvanger, naam, adres, woonplaats) VALUES (1, \"Henk\", \"Hoofdstraat\", \"Hoofddorp\");";
+            String afzenderInsert = "INSERT INTO afzender (idafzender, naam, adres, email, woonplaats) VALUES (1, \"Pietje\", \"Pietjestraat\", \"pietje@gmail.com\", \"Pietdorp\");";
+            String pakketInsert = "INSERT INTO pakket (idpakket, lengte, breedte, hoogte, gewicht, afzender_idafzender, ontvanger_idontvanger) VALUES (1, 50, 50, 50, 500, 1, 1);";
+            
+            //Selects
+            String ontvangerSelect = "SELECT * FROM ontvanger;";
+            String pakketSelect = "SELECT * FROM pakket;";
+            
             try {
                 Statement st = conn.createStatement();
-                //int rs = st.executeUpdate(query);
-                ResultSet rs = st.executeQuery(query);
+//                int rs = st.executeUpdate(pakketInsert);
+                ResultSet rs = st.executeQuery(pakketSelect);
                 while (rs.next()) {
-                    String a = rs.getString("voornaam");
-                    String c = rs.getString("achternaam");
-                    System.out.println(a + " " + c);
+                    //Ontvanger
+//                    String a = rs.getString("naam");
+//                    String b = rs.getString("adres");
+//                    String c = rs.getString("woonplaats");
+                    //Pakket
+                    String a = rs.getString("idpakket");
+                    String b = rs.getString("lengte");
+                    String c = rs.getString("breedte");
+                    String d = rs.getString("hoogte");
+                    String e = rs.getString("gewicht");
+                    
+                    System.out.println(a + " " + b + " " + c + " " + d + " " + e);
                 }
             } catch (SQLException ex) {
                 System.err.println(ex.getMessage());
@@ -37,5 +53,4 @@ public class Test {
             System.err.println(ex.getMessage());
         }
     }
-
 }
--- HSR01JavaFX/src/Config/config.java
+++ HSR01JavaFX/src/Config/config.java
@@ -3,7 +3,7 @@ package Config;
 public class config {
     //Attributes
     public static String DRIVER = "com.mysql.jdbc.Driver";
-    public static String URL = "";
-    public static String USERNAME = "";
-    public static String PASSWORD = "";
+    public static String URL = "jdbc:mysql://michelvaartjes.nl/micheic28_tztdb";
+    public static String USERNAME = "micheic28_tzt";
+    public static String PASSWORD = "tztuserkek420";
 }
